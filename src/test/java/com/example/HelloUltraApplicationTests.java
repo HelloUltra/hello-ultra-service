87b3510df6bb1e12b08cb7fa4078a28c78e6e6ea
권용근 <kingbbode@gmail.com>
2017-03-01 17:31:36 +0900

    MessageDispatcher 테스트 케이스 작성

--- src/test/java/com/example/HelloUltraApplicationTests.java
+++ src/test/java/com/example/HelloUltraApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import com.example.dto.MessageRequest;
+import com.example.repository.TagRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +13,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class HelloUltraApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Autowired
+	private MessageDispatcher dispatcher;
+
+	private MessageRequest messageRequest;
+
+	@Before
+	public void setUp() throws Exception {
+		messageRequest = new MessageRequest();
 	}
 
+	@Test
+	public void Dispatcher_검색기능_테스트() throws Exception {
+		messageRequest.setContent("#검색 spring");
+		System.out.println(dispatcher.dispatch(messageRequest).getText());
+	}
 }
