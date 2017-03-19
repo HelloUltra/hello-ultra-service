d52fcb4007c031f99658472f74f3103a81268b3d
JoongHyun Choi <joonghyun0117@gmail.com>
2017-03-19 23:09:14 +0900

    Redis 설정 추가.

---
+++ src/main/java/com/example/config/AspectConfig.java
@@ -0,0 +1,14 @@
+package com.example.config;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by joonghyun on 2017. 3. 19..
+ */
+@Aspect
+@Component
+public class AspectConfig {
+
+    //TODO redis 작업 처리.
+}
---
+++ src/main/java/com/example/model/Redis.java
@@ -0,0 +1,13 @@
+package com.example.model;
+
+import java.util.Map;
+
+/**
+ * Created by joonghyun on 2017. 3. 19..
+ */
+public class Redis {
+
+    private String status;
+
+    private Map<String, String> data;
+}
